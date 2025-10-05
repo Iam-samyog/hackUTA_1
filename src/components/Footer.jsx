2bc734d818d613b5cb2e3360eb7416a7ab34b37f
Samyog Maharjan <msamyog37@gmail.com>
2025-10-05 05:54:41 -0500

    Improved the code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                     </svg>
                                 </div>
-                                <span className="text-xl font-poppins font-bold">NoteLens</span>
+                                <span className="text-xl font-poppins font-bold">GIST</span>
                             </div>
                             <p className="text-blue-200">Transforming notes into handwritten brilliance.</p>
                         </div>
